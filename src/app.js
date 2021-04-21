d09ea8e110f9d6e831d9ca41a9251b52fa5f786d
leandro3742 <leandro.marrero@outlook.com>
2021-04-21 20:00:50 +0000

    trabajo terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
 /* eslint-disable */
+import validator from "validator";
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let alerta = document.getElementById("alerta");
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  alerta.style.display = "none";
 };
+
+let form = document.getElementById("form");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  let card = document.getElementById("card");
+  let cvc = document.getElementById("cvc");
+  let amount = document.getElementById("amount");
+  let firstName = document.getElementById("name");
+  let lastName = document.getElementById("lastName");
+  let city = document.getElementById("city");
+  let state = document.getElementById("state");
+
+  if (!validator.isCreditCard(card.value)) {
+    card.style.backgroundColor = "#FFA8A8";
+    alerta.style.display = "block";
+  }
+  if (validator.isEmpty(cvc.value)) {
+    cvc.style.backgroundColor = "#FFA8A8";
+    alerta.style.display = "block";
+  }
+  if (validator.isEmpty(amount.value)) {
+    amount.style.backgroundColor = "#FFA8A8";
+    alerta.style.display = "block";
+  }
+  if (validator.isEmpty(firstName.value)) {
+    firstName.style.backgroundColor = "#FFA8A8";
+    alerta.style.display = "block";
+  }
+  if (validator.isEmpty(lastName.value)) {
+    lastName.style.backgroundColor = "#FFA8A8";
+    alerta.style.display = "block";
+  }
+  if (validator.isEmpty(lastName.value)) {
+    lastName.style.backgroundColor = "#FFA8A8";
+    alerta.style.display = "block";
+  }
+  if (validator.isEmpty(city.value)) {
+    city.style.backgroundColor = "#FFA8A8";
+    alerta.style.display = "block";
+  }
+  if (validator.isEmpty(state.value)) {
+    state.style.backgroundColor = "#FFA8A8";
+    alerta.style.display = "block";
+  }
+});
